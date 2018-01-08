59d09cf166c9a0a51dc781866107d8a1fabba93e
Aaron Bach <bachya1208@gmail.com>
2018-01-08 11:11:03 -0700

    Fixed bug with unnecessary/missing import

--- pyden/__init__.py
+++ pyden/__init__.py
@@ -1,10 +1,6 @@
-"""
-File: __init__.py
-Author: Aaron Bach
-Email: bachya1208@gmail.com
-Github: https://github.com/bachya
-"""
+"""Define module-level imports."""
 
-from .__version__ import __author__, __author_email__, __copyright__, __license__ # noqa
-from .__version__ import __description__, __title__, __url__, __version__ # noqa
+# pylint: disable=wildcard-import
+
+from .__version__ import __version__  # noqa
 from .trash import TrashClient # noqa
